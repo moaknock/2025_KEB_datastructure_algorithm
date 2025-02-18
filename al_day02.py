1b7f106181bcd0d6e1df263110b5a3b31f897f62
unknown <jjieun@inha.edu>
2025-02-18 11:37:40 +0900

    v0.2_alday2

--- al_day02.py
+++ al_day02.py
@@ -1,32 +1,23 @@
-class Stack:
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
     def __init__(self):
-        self.items = list()
+        self.head = None
 
-    def push(self, data):
-        self.items.append(data)
+    def append(self,data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self. head
+        while current.next: # if next node exist
+            current = current.next #move
+        current.next = Node(data)
 
-    def pop(self):
-        if self.is_empty():
-            return "Stack is empty"
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def peek(self):
-        if self.is_empty():
-            return "Stack is empty"
-        return self.items[-1]
-
-    def is_empty(self):
-        return len(self.items) == 0  # 스택이 비어 있는지 확인
-
-
-s1 = Stack()
-s1.push(-9)
-s1.push(11)
-s1.push(977)
-
-print(s1.pop())  # 977
-print(s1.peek()) # 11
-print(s1.peek()) # 11 (peek()은 값 제거 없이 확인만 함)
+if __name__ == "__main__":
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
