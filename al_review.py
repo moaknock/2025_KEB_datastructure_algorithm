930d44e35169ccf5b293ecacd4f7b2ba20e43de6
unknown <jjieun@inha.edu>
2025-02-19 13:44:06 +0900

    v0.2_alre

--- al_review.py
+++ al_review.py
@@ -1,13 +1,6 @@
-# numbers = [1,2,3,4,5]
-# for i in numbers:
-#     for j in numbers:
-#         for h in numbers:
-#             x = i + j + h
-#             print(x)
-
-pin = 931
-# n = len(pin)
-n =  len(str(pin))
-for i in range(10**n):
-    if i == pin:
-        print(i)
+x = 1
+n = 5
+a_list = []
+for i in range(1, n + 1):
+    a_list.append(x)
+    x = x * i
